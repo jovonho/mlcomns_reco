52e5d578b1efb9bd86a4f532e0ae8b08242b7729
Loic Ho-Von <loic.hovon@gmail.com>
2022-08-07 14:19:52 -0400

    training script, npint64 to int bugfix

--- pytorch/start_training.sh
+++ pytorch/start_training.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run --gpus all -it --ipc=host --network=host -v /raid/data/reco:/data/cache reco:latest /bin/bash start_training.sh 0
+docker run --gpus all -it --ipc=host --network=host -v /raid/data/reco:/data/cache reco:latest /bin/bash run_and_time.sh 0
